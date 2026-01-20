49b5ba95c16f60cdab3b2b454263b80ec579c408
ZarifulinR <fred-c112@yandex.ru>
2026-01-20 20:48:15 +0300

    ClasssW

---
+++ src/Main.kt
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val myar = arrayOf(1..10);
+    for (i in 1..10 ) {
+       if (i%2 == 0) {
+           println(i);
+       }
+
+    }
+    val myarr2 = arrayOf(-1 ,0,1,2,3,4,20)
+    for (i in -1 ..20) {
+       when{
+            i < 0 -> println("negativr");
+        }
+    }
+
+
+
+    val n = readln().toInt();
+    val myarr3 = arrayOf(n)
+    for (i in myarr3 ) {
+        myarr3
+    }
+}
